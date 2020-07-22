2ea2716b81d7fc1b6096cf970a1d0cdf6e91440d
Sebastian Czech <sebaczech@gmail.com>
2020-07-23 00:19:43 +0200

    test: Simple parameterized test

--- design-patters-creational/src/test/java/com/sebastianczech/AbstractFactoryTest.java
+++ design-patters-creational/src/test/java/com/sebastianczech/AbstractFactoryTest.java
@@ -2,6 +2,9 @@ package com.sebastianczech;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,5 +23,22 @@ public class AbstractFactoryTest {
 
         assertThat(1).isEqualTo(1);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 21, 14, 49 })
+    public void should_return_number_greater_by_one_for_many_parameters(int number) {
+        int output = number + a;
+
+        assertThat(output).isEqualTo(number + 1);
+    }
+
+    @ParameterizedTest(name = "Should return {1} for {0}")
+    @CsvSource(value = {"10:11", "21:22"}, delimiter = ':')
+    void should_return_sum_of_numbers_for_given_input_numbers(int number, int expected) {
+        int output = number + a;
+
+        assertThat(output).isEqualTo(expected);
+    }
+
 
 }
